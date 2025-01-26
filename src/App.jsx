45fa56c5ea8c433277eeccc588e44e18baa9a83c
Lina jamal <linahjamal89@gmail.com>
2025-01-26 11:33:21 +0200

    initial structure

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {  Route, Routes } from "react-router";
+import Layout from "./layout";
+import { HomePage } from "./pages";
+import './theme'
+function App() {
+  return (
+
+      <Routes>
+
+        <Route   element={<Layout />}>
+          <Route index element={<HomePage />} />
+
+
+        </Route>
+      </Routes>
+ 
+  );
+}
+
+export default App;
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div>
+      Hello lofy
+    </div>
+  )
+}
+
+export default HomePage
