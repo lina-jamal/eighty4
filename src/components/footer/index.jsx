59c14de52321dd72806154b441cbde64397fedc8
lina-jamal <linahjamal89@gmail.com>
2025-03-04 16:44:35 +0200

    make the footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,8 +1,43 @@
 import React from "react";
-import { Wrapper } from "./style";
+import { FooterContent, FooterEnd, FooterSection, Wrapper } from "./style";
 
 function Footer() {
-  return <Wrapper>Footer</Wrapper>;
+  return <Wrapper>
+<FooterSection>
+<FooterContent>
+<span>Eighty4</span>
+<p>Explore majestic Sri Lanka with our epic travel packages, from bustling cities to breathtaking beaches and landscapes!</p>
+
+    </FooterContent> <FooterContent>
+<span>Hours Work</span>
+<p>
+Monday - Friday <br />
+9am - 5pm
+</p>
+
+    </FooterContent> <FooterContent>
+<span>Location</span>
+<p>Corporate Office:<br /> 310, Deans Road,<br /> Colombo 01000, Sri Lanka<br /><br />
+Reg. Office:<br /> 60, Mosque Road,<br /> Beruwala 12070, Sri Lanka
+
+</p>
+
+    </FooterContent> <FooterContent>
+<span>Contact Info</span>
+<p>
+hi@eighty4.global <br />
++94 7779 79601
+
+
+</p>
+
+    </FooterContent>
+</FooterSection>
+
+    <FooterEnd>
+      <p>Copyright @ Eighty4 2024. All Rights Reserved.</p>
+    </FooterEnd>
+  </Wrapper>;
 }
 
 export default Footer;
--- src/components/footer/style.jsx
+++ src/components/footer/style.jsx
@@ -1,5 +1,54 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.footer`
-  background-color: blue;
+  background-color: #1D231F;
+  height: 50vh;
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  flex-direction: column;
+
+  gap:2rem;
+`;
+export const FooterSection = styled.div`
+display: flex;
+gap:3rem;
+
+
+
+`;
+export const FooterContent = styled.div`
+display: flex;
+flex-direction: column;
+align-items: start;
+justify-content: center;
+width: 20%;
+color:#FFFFFF;
+gap:1rem;
+
+span {
+   font-size: 1.4rem;
+   line-height: 2rem;
+   font-weight: 400;
+
+    font-family: "EB Garamond"; 
+  }
+   p{
+
+    font-size: .8rem;
+   line-height: 1.5rem;
+   font-weight: 300;
+    font-family: "Roboto";
+  } 
+
+`;
+export const FooterEnd = styled.div`
+  font-size: .7rem;
+color:#FFFFFF;
+  
+   font-weight: 300;
+    font-family: "Roboto";
+  
 `;
